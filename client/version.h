9a6d25ad5e3c9786bc547a34909cc2f6946bc174
zipper <zipper0@protonmail.com>
2020-06-27 22:15:58 +0300

    Bump version to 0.2

--- client/version.h
+++ client/version.h
@@ -24,7 +24,7 @@
 #define APPNAME "blacklink"
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 1
+#define VERSION_MINOR 2
 
 #define VER_STRINGIZE1(a) #a
 #define VER_STRINGIZE(a) VER_STRINGIZE1(a)
