630af915fd5310ec06d47c00e406823412d6776b
zipper <zipper0@protonmail.com>
2020-07-19 14:18:39 +0300

    Move UCHandler code to a cpp file

---
+++ windows/UCHandler.cpp
@@ -0,0 +1,157 @@
+#include "stdafx.h"
+#include "UCHandler.h"
+#include "WinUtil.h"
+#include "../client/FavoriteManager.h"
+
+void UCHandlerBase::appendUcMenu(OMenu& menu, int ctx, const StringList& hubs)
+{
+	FavoriteManager::getInstance()->getUserCommands(userCommands, ctx, hubs);
+
+	const bool useSubMenu = BOOLSETTING(UC_SUBMENU);
+	const int prevCount = menu.GetMenuItemCount();
+	menuPos = prevCount;
+
+	bool addOpCommands = ctx != UserCommand::CONTEXT_HUB && ctx != UserCommand::CONTEXT_SEARCH && ctx != UserCommand::CONTEXT_FILELIST;
+	if (addOpCommands)
+	{
+		for (int i = 0; i < prevCount; i++)
+			if (menu.GetMenuItemID(i) == IDC_REPORT)
+			{
+				addOpCommands = false;
+				break;
+			}
+		if (addOpCommands)
+		{
+			if (prevCount) menu.AppendMenu(MF_SEPARATOR);
+			menu.AppendMenu(MF_STRING, IDC_GET_USER_RESPONSES, CTSTRING(GET_USER_RESPONSES));
+			menu.AppendMenu(MF_STRING, IDC_REPORT, CTSTRING(DUMP_USER_INFO));
+#ifdef IRAINMAN_INCLUDE_USER_CHECK
+			menu.AppendMenu(MF_STRING, IDC_CHECKLIST, CTSTRING(CHECK_FILELIST));
+#endif
+		}
+	}
+
+	int subMenuIndex = -1;
+	if (useSubMenu)
+	{
+		if (!subMenu.m_hMenu)
+			subMenu.CreatePopupMenu();
+		else
+			subMenu.ClearMenu();
+		subMenu.InsertSeparatorFirst(TSTRING(USER_COMMANDS));
+		WinUtil::appendSeparator(menu);
+		subMenuIndex = menu.GetMenuItemCount();
+		menu.AppendMenu(MF_POPUP, (HMENU) subMenu, CTSTRING(USER_COMMANDS), g_iconBitmaps.bitmaps[IconBitmaps::BITMAP_COMMANDS]);
+	}
+
+	CMenuHandle cur = useSubMenu ? subMenu.m_hMenu : menu.m_hMenu;
+	OMenu* menus[] = { &menu, &subMenu, nullptr };
+
+	constexpr size_t MAX_TEXT_LEN = 511;
+	bool firstCommand = true;
+	for (size_t n = 0; n < userCommands.size(); ++n)
+	{
+		UserCommand& uc = userCommands[n];
+		if (uc.getType() == UserCommand::TYPE_SEPARATOR)
+			appendSeparator(menus, cur);
+		if (uc.getType() == UserCommand::TYPE_RAW || uc.getType() == UserCommand::TYPE_RAW_ONCE)
+		{
+			tstring name;
+			const StringList displayName = uc.getDisplayName();
+			cur = useSubMenu ? subMenu.m_hMenu : menu.m_hMenu;
+			for (size_t i = 0; i < displayName.size(); ++i)
+			{
+				Text::toT(displayName[i], name);
+				if (name.length() > MAX_TEXT_LEN) name.erase(MAX_TEXT_LEN);
+				if (i + 1 == displayName.size())
+				{
+					if (firstCommand && !useSubMenu && i == 0)
+					{
+						appendSeparator(menus, cur);
+						firstCommand = false;
+					}
+					appendMenu(menus, cur, MF_STRING, IDC_USER_COMMAND + n, name);
+				}
+				else
+				{
+					bool found = false;
+					TCHAR buf[MAX_TEXT_LEN + 1];
+					// Let's see if we find an existing item...
+					int count = cur.GetMenuItemCount();
+					for (int k = 0; k < count; k++)
+					{
+						if (cur.GetMenuState(k, MF_BYPOSITION) & MF_POPUP)
+						{
+							cur.GetMenuString(k, buf, _countof(buf), MF_BYPOSITION);
+							if (stricmp(buf, name) == 0)
+							{
+								found = true;
+								cur = (HMENU) cur.GetSubMenu(k);
+								break;
+							}
+						}
+					}
+					if (!found)
+					{
+						if (firstCommand && !useSubMenu && i == 0)
+						{
+							appendSeparator(menus, cur);
+							firstCommand = false;
+						}
+						HMENU newSubMenu = CreatePopupMenu();
+						appendMenu(menus, cur, MF_POPUP, (UINT_PTR) newSubMenu, name);
+						cur = newSubMenu;
+					}
+				}
+			}
+		}
+	}
+	if (subMenuIndex != -1 && userCommands.empty())
+		menu.EnableMenuItem(subMenuIndex, MF_BYPOSITION | MF_DISABLED | MF_GRAYED);
+	insertedItems = menu.GetMenuItemCount() - prevCount;
+}
+
+void UCHandlerBase::cleanUcMenu(OMenu& menu)
+{
+	MENUITEMINFO mii = { sizeof(mii) };
+	mii.fMask = MIIM_SUBMENU;
+	while (insertedItems)
+	{
+		menu.GetMenuItemInfo(menuPos, TRUE, &mii);
+		if (mii.hSubMenu == subMenu)
+			menu.RemoveMenu(menuPos, MF_BYPOSITION);
+		else
+			menu.DeleteMenu(menuPos, MF_BYPOSITION);
+		insertedItems--;
+	}
+}
+
+void UCHandlerBase::appendMenu(OMenu** oldMenu, CMenuHandle& menu, UINT flags, UINT_PTR id, const tstring& text)
+{
+	int index = 0;
+	while (oldMenu[index])
+	{
+		if ((HMENU) *oldMenu[index] == (HMENU) menu)
+		{
+			oldMenu[index]->AppendMenu(flags, id, text.c_str());
+			return;
+		}
+		++index;
+	}
+	menu.AppendMenu(flags, id, text.c_str());
+}
+
+void UCHandlerBase::appendSeparator(OMenu** oldMenu, CMenuHandle& menu)
+{
+	int index = 0;
+	while (oldMenu[index])
+	{
+		if ((HMENU) *oldMenu[index] == (HMENU) menu)
+		{
+			WinUtil::appendSeparator(*oldMenu[index]);
+			return;
+		}
+		++index;
+	}
+	WinUtil::appendSeparator(menu);
+}
--- windows/UCHandler.h
+++ windows/UCHandler.h
@@ -19,19 +19,40 @@
 #ifndef UC_HANDLER_H
 #define UC_HANDLER_H
 
-#include "../client/FavoriteManager.h"
-#include "../client/ClientManager.h"
 #include "OMenu.h"
-#include "WinUtil.h"
+#include "UserCommand.h"
+#include "resource.h"
 
-template<class T>
-class UCHandler
+class UCHandlerBase
 {
 	public:
-		UCHandler() : menuPos(0), insertedItems(0)
+		UCHandlerBase() : menuPos(0), insertedItems(0)
 		{
 		}
-		
+
+		void appendUcMenu(OMenu& menu, int ctx, const StringList& hubs);
+		void appendUcMenu(OMenu& menu, int ctx, const string& hubUrl)
+		{
+			appendUcMenu(menu, ctx, StringList(1, hubUrl));
+		}
+		void cleanUcMenu(OMenu& menu);
+
+	protected:
+		vector<UserCommand> userCommands;
+
+	private:
+		OMenu subMenu;
+		int menuPos;
+		int insertedItems;
+
+		void appendMenu(OMenu** oldMenu, CMenuHandle& menu, UINT flags, UINT_PTR id, const tstring& text);
+		void appendSeparator(OMenu** oldMenu, CMenuHandle& menu);
+};
+
+template<class T>
+class UCHandler : public UCHandlerBase
+{
+	public:
 		typedef UCHandler<T> thisClass;
 		BEGIN_MSG_MAP(thisClass)
 		COMMAND_RANGE_HANDLER(IDC_USER_COMMAND, IDC_USER_COMMAND + userCommands.size(), onUserCommand)
@@ -40,7 +61,7 @@ class UCHandler
 		LRESULT onUserCommand(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 		{
 			dcassert(wID >= IDC_USER_COMMAND);
-			size_t n = (size_t)wID - IDC_USER_COMMAND;
+			unsigned n = wID - IDC_USER_COMMAND;
 			dcassert(n < userCommands.size());
 			
 			UserCommand& uc = userCommands[n];
@@ -49,170 +70,6 @@ class UCHandler
 			
 			t->runUserCommand(uc);
 			return 0;
-		}
-		
-		void appendUcMenu(OMenu& menu, int ctx, const string& hubUrl)
-		{
-			appendUcMenu(menu, ctx, StringList(1, hubUrl));
-		}
-		
-		void appendUcMenu(OMenu& menu, int ctx, const StringList& hubs)
-		{
-			FavoriteManager::getInstance()->getUserCommands(userCommands, ctx, hubs);
-
-			const bool useSubMenu = BOOLSETTING(UC_SUBMENU);
-			const int prevCount = menu.GetMenuItemCount();
-			menuPos = prevCount;
-			
-			bool addOpCommands = ctx != UserCommand::CONTEXT_HUB && ctx != UserCommand::CONTEXT_SEARCH && ctx != UserCommand::CONTEXT_FILELIST;
-			if (addOpCommands)
-			{
-				for (int i = 0; i < prevCount; i++)
-					if (menu.GetMenuItemID(i) == IDC_REPORT)
-					{
-						addOpCommands = false;
-						break;
-					}
-				if (addOpCommands)
-				{
-					if (prevCount) menu.AppendMenu(MF_SEPARATOR);
-					menu.AppendMenu(MF_STRING, IDC_GET_USER_RESPONSES, CTSTRING(GET_USER_RESPONSES));
-					menu.AppendMenu(MF_STRING, IDC_REPORT, CTSTRING(DUMP_USER_INFO));
-#ifdef IRAINMAN_INCLUDE_USER_CHECK
-					menu.AppendMenu(MF_STRING, IDC_CHECKLIST, CTSTRING(CHECK_FILELIST));
-#endif
-				}
-			}
-
-			int subMenuIndex = -1;
-			if (useSubMenu)
-			{
-				if (!subMenu.m_hMenu)
-					subMenu.CreatePopupMenu();
-				else
-					subMenu.ClearMenu();
-				subMenu.InsertSeparatorFirst(TSTRING(USER_COMMANDS));
-				WinUtil::appendSeparator(menu);
-				subMenuIndex = menu.GetMenuItemCount();
-				menu.AppendMenu(MF_POPUP, (HMENU) subMenu, CTSTRING(USER_COMMANDS), g_iconBitmaps.bitmaps[IconBitmaps::BITMAP_COMMANDS]);
-			}
-								
-			CMenuHandle cur = useSubMenu ? subMenu.m_hMenu : menu.m_hMenu;
-			OMenu* menus[] = { &menu, &subMenu, nullptr };
-				
-			constexpr size_t MAX_TEXT_LEN = 511;
-			bool firstCommand = true;
-			for (size_t n = 0; n < userCommands.size(); ++n)
-			{
-				UserCommand& uc = userCommands[n];
-				if (uc.getType() == UserCommand::TYPE_SEPARATOR)
-					appendSeparator(menus, cur);
-				if (uc.getType() == UserCommand::TYPE_RAW || uc.getType() == UserCommand::TYPE_RAW_ONCE)
-				{
-					tstring name;
-					const StringList displayName = uc.getDisplayName();
-					cur = useSubMenu ? subMenu.m_hMenu : menu.m_hMenu;
-					for (size_t i = 0; i < displayName.size(); ++i)
-					{
-						Text::toT(displayName[i], name);
-						if (name.length() > MAX_TEXT_LEN) name.erase(MAX_TEXT_LEN);
-						if (i + 1 == displayName.size())
-						{
-							if (firstCommand && !useSubMenu && i == 0)
-							{
-								appendSeparator(menus, cur);
-								firstCommand = false;
-							}
-							appendMenu(menus, cur, MF_STRING, IDC_USER_COMMAND + n, name);
-						}
-						else
-						{
-							bool found = false;
-							TCHAR buf[MAX_TEXT_LEN + 1];
-							// Let's see if we find an existing item...
-							int count = cur.GetMenuItemCount();
-							for (int k = 0; k < count; k++)
-							{
-								if (cur.GetMenuState(k, MF_BYPOSITION) & MF_POPUP)
-								{
-									cur.GetMenuString(k, buf, _countof(buf), MF_BYPOSITION);
-									if (stricmp(buf, name) == 0)
-									{
-										found = true;
-										cur = (HMENU) cur.GetSubMenu(k);
-										break;
-									}
-								}
-							}
-							if (!found)
-							{
-								if (firstCommand && !useSubMenu && i == 0)
-								{
-									appendSeparator(menus, cur);
-									firstCommand = false;
-								}
-								HMENU newSubMenu = CreatePopupMenu();
-								appendMenu(menus, cur, MF_POPUP, (UINT_PTR) newSubMenu, name);
-								cur = newSubMenu;
-							}
-						}
-					}
-				}
-			}
-			if (subMenuIndex != -1 && userCommands.empty())
-				menu.EnableMenuItem(subMenuIndex, MF_BYPOSITION | MF_DISABLED | MF_GRAYED);
-			insertedItems = menu.GetMenuItemCount() - prevCount;
-		}
-
-		void cleanUcMenu(OMenu& menu)
-		{
-			MENUITEMINFO mii = { sizeof(mii) };
-			mii.fMask = MIIM_SUBMENU;
-			while (insertedItems)
-			{
-				menu.GetMenuItemInfo(menuPos, TRUE, &mii);
-				if (mii.hSubMenu == subMenu)
-					menu.RemoveMenu(menuPos, MF_BYPOSITION);
-				else
-					menu.DeleteMenu(menuPos, MF_BYPOSITION);
-				insertedItems--;
-			}
-		}
-
-	private:
-		vector<UserCommand> userCommands;
-		OMenu subMenu;
-		int menuPos;
-		int insertedItems;
-
-		void appendMenu(OMenu** oldMenu, CMenuHandle& menu, UINT flags, UINT_PTR id, const tstring& text)
-		{
-			int index = 0;
-			while (oldMenu[index])
-			{
-				if ((HMENU) *oldMenu[index] == (HMENU) menu)
-				{
-					oldMenu[index]->AppendMenu(flags, id, text.c_str());
-					return;
-				}
-				++index;
-			}
-			menu.AppendMenu(flags, id, text.c_str());
-		}
-
-		void appendSeparator(OMenu** oldMenu, CMenuHandle& menu)
-		{
-			int index = 0;
-			while (oldMenu[index])
-			{
-				if ((HMENU) *oldMenu[index] == (HMENU) menu)
-				{
-					WinUtil::appendSeparator(*oldMenu[index]);
-					return;
-				}
-				++index;
-			}
-			WinUtil::appendSeparator(menu);
 		}
 };
 
