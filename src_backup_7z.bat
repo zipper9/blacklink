2d53ae8a5c531d495f8ae174d5ab9c80afaf7fed
zipper <zipper0@protonmail.com>
2020-09-26 21:43:07 +0300

    Update backup script

--- src_backup_7z.bat
+++ src_backup_7z.bat
@@ -1,9 +1,11 @@
+@echo off
 
-call src_gen_filename.bat -src
-7z.exe a -r -t7z -m0=lzma -mx=9 -mfb=512 -md=1024m -ms=on fly-backup-%FILE_NAME%.7z -i@src_include.txt -x@src_exclude.txt
+for /f %%# in ('WMIC Path Win32_LocalTime Get /Format:value') do @for /f %%@ in ("%%#") do @set %%@
 
-goto :end
-:error
-echo Some error occured
-pause
-:end
+set "month=0%month%"
+set "month=%month:~-2%"
+
+set "day=0%day%"
+set "day=%day:~-2%"
+
+"C:\Program Files\7-Zip\7z.exe" a -r -t7z -m0=lzma -mx=9 -mfb=512 -md=1024m -ms=on blacklink_backup_%year%%month%%day%.7z -i@src_include.txt -x@src_exclude.txt
