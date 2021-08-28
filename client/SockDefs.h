bc997464858355f996a07f29faf345b42909a11c
zipper <zipper0@protonmail.com>
2021-08-28 20:17:58 +0300

    Fix SockDefs.h

--- client/SockDefs.h
+++ client/SockDefs.h
@@ -2,7 +2,7 @@
 #define SOCK_DEFS_H_
 
 #ifdef _WIN32
-#if defined(_WIN32_WINNT) && _WIN32_WINNT < 0x0600
+#if !defined(_WIN32_WINNT) || _WIN32_WINNT < 0x0600
 #include <winsock2.h>
 #else
 #include <ws2def.h>
