zipper9/blacklink

commit 17533ff3b9adce4388c1e9ff6f4b0284b901898c

Fix firing of ClientListener::LoggedIn notification on ADC hubs

client/AdcHub.cpp
@@ -107,12 +107,18 @@ void AdcHub::getUserList(OnlineUserList& result) const
 	}
 }
 
-OnlineUserPtr AdcHub::getUser(const uint32_t sid, const CID& cid, const string& nick)
+OnlineUserPtr AdcHub::getUser(uint32_t sid, const CID& cid, const string& nick)
 {
 	OnlineUserPtr ou = findUser(sid);
 	if (ou)
 		return ou;
-		
+
+	return addUser(sid, cid, nick);
+}
+
+OnlineUserPtr AdcHub::addUser(uint32_t sid, const CID& cid, const string& nick)
+{
+	OnlineUserPtr ou;
 	if (cid.isZero())
 	{
 		WRITE_LOCK(*csUsers);
@@ -126,8 +132,6 @@ OnlineUserPtr AdcHub::getUser(const uint32_t sid, const CID& cid, const string& 
 		}
 		ou->getIdentity().setSID(sid);
 		ou->getUser()->addNick(nick, getHubUrl());
-		if (ou->getIdentity().isOp())
-			fire(ClientListener::HubInfoMessage(), ClientListener::LoggedIn, this, Util::emptyString);
 	}
 	else // User
 	{
@@ -137,7 +141,7 @@ OnlineUserPtr AdcHub::getUser(const uint32_t sid, const CID& cid, const string& 
 		WRITE_LOCK(*csUsers);
 		ou = users.insert(make_pair(sid, newUser)).first->second;
 	}
-	
+
 	if (sid != AdcCommand::HUB_SID)
 	{
 		ClientManager::getInstance()->putOnline(ou, true);
@@ -161,7 +165,7 @@ OnlineUserPtr AdcHub::findUser(const string& nick) const
 	return nullptr;
 }
 
-OnlineUserPtr AdcHub::findUser(const uint32_t sid) const
+OnlineUserPtr AdcHub::findUser(uint32_t sid) const
 {
 	READ_LOCK(*csUsers);
 	const auto& i = users.find(sid);
@@ -181,7 +185,7 @@ OnlineUserPtr AdcHub::findUser(const CID& cid) const
 	return nullptr;
 }
 
-void AdcHub::putUser(const uint32_t sid, bool disconnect)
+void AdcHub::putUser(uint32_t sid, bool disconnect)
 {
 	OnlineUserPtr ou;
 	{
@@ -234,9 +238,9 @@ void AdcHub::clearUsers()
 
 void AdcHub::handle(AdcCommand::INF, const AdcCommand& c) noexcept
 {
-	if (c.getParameters().empty())
-		return;
-	OnlineUserPtr ou; // [!] IRainman fix: use OnlineUserPtr here!
+	if (c.getParameters().empty()) return;
+	OnlineUserPtr ou;
+	bool newUser = false;
 	string cidStr;
 	if (c.getParam("ID", 0, cidStr))
 	{
@@ -257,7 +261,12 @@ void AdcHub::handle(AdcCommand::INF, const AdcCommand& c) noexcept
 		}
 		else
 		{
-			ou = getUser(c.getFrom(), cid, c.getNick());
+			ou = findUser(sid);
+			if (!ou)
+			{
+				newUser = true;
+				ou = addUser(c.getFrom(), cid, c.getNick());
+			}
 		}
 	}
 	else if (c.getFrom() == AdcCommand::HUB_SID)
@@ -429,7 +438,7 @@ void AdcHub::handle(AdcCommand::INF, const AdcCommand& c) noexcept
 		if (Util::parseIpAddress(ip, ip6) && Util::isValidIp6(ip))
 			id.setIP6(ip);
 	}
-	
+
 	if (isMe(ou))
 	{
 		csState.lock();
@@ -439,6 +448,8 @@ void AdcHub::handle(AdcCommand::INF, const AdcCommand& c) noexcept
 		setAutoReconnect(true);
 		updateCounts(false);
 		fireUserUpdated(ou);
+		if (newUser && ou->getIdentity().isOp())
+			fire(ClientListener::HubInfoMessage(), ClientListener::LoggedIn, this, Util::emptyString);
 	}
 	else if (ou->getIdentity().isHub())
 	{

client/AdcHub.h
@@ -99,31 +99,32 @@ class AdcHub : public Client, public CommandHandler<AdcHub>
 		StringMap lastInfoMap;
 
 		void addInfoParam(AdcCommand& c, const string& var, const string& value);
-		
+
 		bool isFeatureSupported(unsigned feature) const
 		{
 			LOCK(csState);
 			return (featureFlags & feature) != 0;
 		}
-		
+
 		string salt;
 		uint32_t sid;
-		
+
 		boost::unordered_set<uint32_t> forbiddenCommands;
-		
+
 		static const vector<StringList> searchExts;
-		
-		OnlineUserPtr getUser(const uint32_t sid, const CID& cid, const string& nick);
-		OnlineUserPtr findUser(const uint32_t sid) const;
+
+		OnlineUserPtr getUser(uint32_t sid, const CID& cid, const string& nick);
+		OnlineUserPtr addUser(uint32_t sid, const CID& cid, const string& nick);
+		OnlineUserPtr findUser(uint32_t sid) const;
 		OnlineUserPtr findUser(const CID& cid) const;
-		
+
 		// just a workaround
 		OnlineUserPtr findUser(const string& nick) const;
-		
-		void putUser(const uint32_t sid, bool disconnect);
-		
+
+		void putUser(uint32_t sid, bool disconnect);
+
 		void clearUsers();
-		
+
 		void handle(AdcCommand::SUP, const AdcCommand& c) noexcept;
 		void handle(AdcCommand::SID, const AdcCommand& c) noexcept;
 		void handle(AdcCommand::MSG, const AdcCommand& c) noexcept;

commit 8f65928f6cd0253a395c390cf07596c7c5da348b

Fix bug introduced in 17533ff

client/AdcHub.cpp
@@ -261,7 +261,7 @@ void AdcHub::handle(AdcCommand::INF, const AdcCommand& c) noexcept
 		}
 		else
 		{
-			ou = findUser(sid);
+			ou = findUser(c.getFrom());
 			if (!ou)
 			{
 				newUser = true;