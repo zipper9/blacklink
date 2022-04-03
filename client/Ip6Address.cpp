zipper9/blacklink

commit 0447f0bbf7b3466ac2bf1f2adee211d4ef8ce1ec

Add Ip6AddressEx (with scopeId), improve network utilities

client/Ip6Address.cpp
@@ -1,6 +1,7 @@
 #include "stdinc.h"
 #include "Ip6Address.h"
 #include "inet_compat.h"
+#include "StrUtil.h"
 
 #ifdef _WIN32
 #include <ws2tcpip.h>
@@ -9,9 +10,20 @@
 #include <netinet/ip6.h>
 #endif
 
-static bool parseIpAddress(Ip6Address& result, const char* s) noexcept
+static inline bool parseIpAddress(Ip6Address& result, const char* s) noexcept
 {
 	return inet_pton_compat(AF_INET6, s, &result) == 1;
+}
+
+static inline bool checkPrefix(uint16_t w, uint16_t prefix, int len) noexcept
+{
+	return (w & (0xFFFF << (16-len))) == prefix;
+}
+
+static inline bool isLinkScopedIp(const Ip6Address& ip) noexcept
+{
+	uint16_t w = ntohs(ip.data[0]);
+	return checkPrefix(w, 0xFE80, 10);
 }
 
 bool Util::parseIpAddress(Ip6Address& result, const string& s, string::size_type start, string::size_type end) noexcept
@@ -25,25 +37,35 @@ bool Util::parseIpAddress(Ip6Address& result, const string& s, string::size_type
 	if (pos != string::npos && pos < end) end = pos;
 	size_t len = end - start;
 	if (start == 0 && len == s.length())
-		return parseIpAddress(result, s.c_str());
-	return parseIpAddress(result, s.substr(start, len).c_str());
+		return ::parseIpAddress(result, s.c_str());
+	return ::parseIpAddress(result, s.substr(start, len).c_str());
 }
 
-bool Util::parseIpAddress(Ip6Address& result, const wstring& s, wstring::size_type start, wstring::size_type end) noexcept
+bool Util::parseIpAddress(Ip6AddressEx& result, const string& s, string::size_type start, string::size_type end) noexcept
 {
-	if (end - start > 2 && s[start] == L'[' && s[end-1] == L']')
+	result.scopeId = 0;
+	if (end - start > 2 && s[start] == '[' && s[end-1] == ']')
 	{
 		start++;
 		end--;
 	}
-	wstring::size_type pos = s.find(L'%', start);
-	if (pos != wstring::npos && pos < end) end = pos;
+	string::size_type endPos = end;
+	string::size_type scopePos = s.find('%', start);
+	if (scopePos != string::npos && scopePos < end)
+	{
+		if (scopePos + 1 == end) return false;
+		end = scopePos++;
+	}
 	size_t len = end - start;
-	string tmp;
-	tmp.resize(len);
-	for (size_t i = 0; i < len; ++i)
-		tmp[i] = (char) s[start + i];
-	return parseIpAddress(result, tmp);
+	if (start == 0 && len == s.length())
+		return ::parseIpAddress(result, s.c_str());
+	if (!::parseIpAddress(result, s.substr(start, len).c_str())) return false;
+	if (scopePos != string::npos && isLinkScopedIp(result))
+	{
+		result.scopeId = Util::stringToInt<uint32_t, char>(s.c_str(), scopePos);
+		if (scopePos != endPos) return false;
+	}
+	return true;
 }
 
 bool Util::isEmpty(const Ip6Address& ip) noexcept
@@ -75,13 +97,12 @@ string Util::printIpAddress(const Ip6Address& ip, bool brackets) noexcept
 	return string(buf, len);
 }
 
-wstring Util::printIpAddressW(const Ip6Address& ip, bool brackets) noexcept
+string Util::printIpAddress(const Ip6AddressEx& ip, bool brackets) noexcept
 {
-	string tmp = printIpAddress(ip, brackets);
-	wstring result;
-	size_t len = tmp.length();
-	result.resize(len);
-	for (size_t i = 0; i < len; ++i)
-		result[i] = tmp[i];
-	return result;
+	if (!ip.scopeId) return printIpAddress(static_cast<const Ip6Address&>(ip));
+	string s = printIpAddress(static_cast<const Ip6Address&>(ip));
+	char buf[64];
+	int len = sprintf(buf, "%%%u", ip.scopeId);
+	s.insert(brackets ? s.length() - 1 : s.length(), buf, len);
+	return s;
 }

client/BufferedSocket.cpp
@@ -702,7 +702,7 @@ void BufferedSocket::createSocksMessage(const BufferedSocket::ConnectInfo* ci)
 			}
 			else
 			{
-				IpAddress addr;
+				IpAddressEx addr;
 				bool isNumeric;
 				if (!Socket::resolveHost(addr, getIp().type, ci->addr, &isNumeric)) // FIXME
 				{
@@ -767,7 +767,7 @@ void BufferedSocket::checkSocksReply()
 	rb.clear();
 	proxyStage = PROXY_STAGE_NONE;
 	mode = MODE_LINE;
-	IpAddress ip;
+	IpAddressEx ip;
 	Socket::resolveHost(ip, getIp().type, connectInfo->addr);
 	sock->setIp(ip);
 	sock->setPort(connectInfo->port);
@@ -828,7 +828,7 @@ void BufferedSocket::doConnect(const BufferedSocket::ConnectInfo* ci, bool sslSo
 					host = &ci->addr;
 					port = ci->port;
 				}
-				IpAddress ip;
+				IpAddressEx ip;
 				bool isNumeric;
 				if (!Socket::resolveHost(ip, ipVersion, *host, &isNumeric))
 				{
@@ -843,7 +843,7 @@ void BufferedSocket::doConnect(const BufferedSocket::ConnectInfo* ci, bool sslSo
 						CryptoManager::getInstance()->getClientSocket(ci->allowUntrusted, ci->expKP, protocol)) : new Socket);
 					s->create(ip.type, Socket::TYPE_TCP);
 					setSocket(move(s));
-					IpAddress bindAddr;
+					IpAddressEx bindAddr;
 					getBindAddress(bindAddr, ip.type);
 					sock->bind(ci->localPort, bindAddr);
 				}
@@ -998,11 +998,11 @@ void BufferedSocket::printSockName(string& sockName) const
 		sockName = "Socket 0";
 }
 
-void BufferedSocket::getBindAddress(IpAddress& ip, int af, const string& s)
+void BufferedSocket::getBindAddress(IpAddressEx& ip, int af, const string& s)
 {
 	if (af == AF_INET6)
 	{
-		Ip6Address v6;
+		Ip6AddressEx v6;
 		if (!s.empty() && Util::parseIpAddress(v6, s))
 			ip.data.v6 = v6;
 		else
@@ -1020,7 +1020,7 @@ void BufferedSocket::getBindAddress(IpAddress& ip, int af, const string& s)
 	}
 }
 
-void BufferedSocket::getBindAddress(IpAddress& ip, int af)
+void BufferedSocket::getBindAddress(IpAddressEx& ip, int af)
 {
 	getBindAddress(ip, af, af == AF_INET6 ? SETTING(BIND_ADDRESS6) : SETTING(BIND_ADDRESS));
 }

client/BufferedSocket.h
@@ -37,8 +37,8 @@ class BufferedSocket : private Thread
 			delete sock;
 		}
 
-		static void getBindAddress(IpAddress& ip, int af, const string& s);
-		static void getBindAddress(IpAddress& ip, int af);
+		static void getBindAddress(IpAddressEx& ip, int af, const string& s);
+		static void getBindAddress(IpAddressEx& ip, int af);
 
 #ifdef FLYLINKDC_USE_SOCKET_COUNTER
 		static void waitShutdown();

client/Client.cpp
@@ -548,11 +548,16 @@ void Client::getLocalIp(Ip4Address& ip4, Ip6Address& ip6) const
 		}
 		if (externalIP.empty() || !BOOLSETTING(NO_IP_OVERRIDE))
 		{
-			string ip = ConnectivityManager::getInstance()->getReflectedIP(AF_INET);
-			if (!ip.empty()) externalIP = std::move(ip);
+			IpAddress ip = ConnectivityManager::getInstance()->getReflectedIP(AF_INET);
+			if (!Util::isEmpty(ip)) externalIP = Util::printIpAddress(ip);
 		}
-		if (externalIP.empty()) externalIP = ConnectivityManager::getInstance()->getLocalIP(AF_INET);
-		Util::parseIpAddress(ip4, externalIP);
+		if (externalIP.empty())
+		{
+			IpAddress ip = ConnectivityManager::getInstance()->getLocalIP(AF_INET);
+			ip4 = ip.data.v4;
+		}
+		else
+			Util::parseIpAddress(ip4, externalIP);
 	}
 
 	if (Util::isEmpty(ip6))
@@ -565,11 +570,16 @@ void Client::getLocalIp(Ip4Address& ip4, Ip6Address& ip6) const
 		}
 		if (externalIP.empty() || !BOOLSETTING(NO_IP_OVERRIDE6))
 		{
-			string ip = ConnectivityManager::getInstance()->getReflectedIP(AF_INET6);
-			if (!ip.empty()) externalIP = std::move(ip);
+			IpAddress ip = ConnectivityManager::getInstance()->getReflectedIP(AF_INET6);
+			if (!Util::isEmpty(ip)) externalIP = Util::printIpAddress(ip);
 		}
-		if (externalIP.empty()) externalIP = ConnectivityManager::getInstance()->getLocalIP(AF_INET6);
-		Util::parseIpAddress(ip6, externalIP);
+		if (externalIP.empty())
+		{
+			IpAddress ip = ConnectivityManager::getInstance()->getLocalIP(AF_INET6);
+			ip6 = ip.data.v6;
+		}
+		else
+			Util::parseIpAddress(ip6, externalIP);
 	}
 }
 

client/ConnectionManager.cpp
@@ -385,7 +385,7 @@ void ConnectionManager::startListen(int af, int type)
 		bind = SettingsManager::get(ips.bindAddress);
 	uint16_t port = SettingsManager::get(portSetting);
 
-	IpAddress bindIp;
+	IpAddressEx bindIp;
 	BufferedSocket::getBindAddress(bindIp, af, bind);
 	auto newServer = new Server(type, bindIp, port);
 	SettingsManager::set(portSetting, newServer->getServerPort());
@@ -786,7 +786,7 @@ void ConnectionManager::on(TimerManagerListener::Minute, uint64_t tick) noexcept
 static const uint64_t g_FLOOD_TRIGGER = 20000;
 static const uint64_t g_FLOOD_ADD = 2000;
 
-ConnectionManager::Server::Server(int type, const IpAddress& ip, uint16_t port): type(type), bindIp(ip), stopFlag(false)
+ConnectionManager::Server::Server(int type, const IpAddressEx& ip, uint16_t port): type(type), bindIp(ip), stopFlag(false)
 {
 	sock.create(ip.type, Socket::TYPE_TCP);
 	sock.setSocketOpt(SOL_SOCKET, SO_REUSEADDR, 1);
@@ -946,11 +946,16 @@ void ConnectionManager::accept(const Socket& sock, int type, Server* server) noe
 void ConnectionManager::updateLocalIp(int af)
 {
 	int index = af == AF_INET6 ? SERVER_V6 : 0;
-	string localIp = servers[index] ? servers[index]->getServerIP() : Util::emptyString;
-	IpAddress addr;
-	if (localIp.empty() || !(Util::parseIpAddress(addr, localIp) && addr.type == af && Util::isValidIp(addr)))
-		localIp = Util::getLocalIp(af);
-	ConnectivityManager::getInstance()->setLocalIP(af, localIp);
+	IpAddress localIp;
+	memset(&localIp, 0, sizeof(localIp));
+	if (servers[index])
+		localIp = servers[index]->getServerIP();
+	if (!Util::isValidIp(localIp))
+	{
+		IpAddressEx ip = Util::getLocalIp(af);
+		memcpy(&localIp, &ip, sizeof(IpAddress));
+	}
+	ConnectivityManager::getInstance()->setLocalIP(localIp);
 }
 
 void ConnectionManager::nmdcConnect(const IpAddress& address, uint16_t port, const string& myNick, const string& hubUrl, int encoding, bool secure)
@@ -1049,9 +1054,11 @@ void ConnectionManager::processMyNick(UserConnection* source, const string& nick
 		if (!nci.hubUrl.empty()) connectNextNmdcUser(nci);
 		hubUrl = std::move(ed.hubUrl);
 	}
+	else
+		hubUrl = source->getHubUrl();
 	
 	const string nickUtf8 = Text::toUtf8(nick, source->getEncoding());
-	const CID cid = ClientManager::makeCid(nickUtf8, source->getHubUrl());
+	const CID cid = ClientManager::makeCid(nickUtf8, hubUrl);
 	
 	// First, we try looking in the pending downloads...hopefully it's one of them...
 	if (!ClientManager::isBeforeShutdown())
@@ -1079,13 +1086,13 @@ void ConnectionManager::processMyNick(UserConnection* source, const string& nick
 		source->setUser(ClientManager::findUser(cid));
 		if (!source->getUser())
 		{
-			LogManager::message("Incoming connection from unknown user " + nickUtf8 + '/' + cid.toBase32() + (hubUrl.empty() ? Util::emptyString : " (" + hubUrl + ")"), false);
+			LogManager::message("Upload: unknown user " + nickUtf8 + '/' + cid.toBase32() + (hubUrl.empty() ? Util::emptyString : " (" + hubUrl + ")"), false);
 			putConnection(source);
 			return;
 		}
 		if (!source->getUser()->isOnline())
 		{
-			LogManager::message("Incoming connection from offline user " + nickUtf8 + '/' + cid.toBase32() + (hubUrl.empty() ? Util::emptyString : " (" + hubUrl + ")"), false);
+			LogManager::message("Upload: offline user " + nickUtf8 + '/' + cid.toBase32() + (hubUrl.empty() ? Util::emptyString : " (" + hubUrl + ")"), false);
 			putConnection(source);
 			return;
 		}

client/ConnectionManager.h
@@ -227,15 +227,15 @@ class ConnectionManager :
 		class Server : public Thread
 		{
 			public:
-				Server(int type, const IpAddress& ip, uint16_t port);
+				Server(int type, const IpAddressEx& ip, uint16_t port);
 				uint16_t getServerPort() const
 				{
 					dcassert(serverPort);
 					return serverPort;
 				}
-				string getServerIP() const
+				IpAddress getServerIP() const
 				{
-					return Util::printIpAddress(sock.getLocalIp());
+					return sock.getLocalIp();
 				}
 				~Server()
 				{
@@ -251,7 +251,7 @@ class ConnectionManager :
 				Socket sock;
 				uint16_t serverPort;
 				const int type;
-				IpAddress bindIp;
+				IpAddressEx bindIp;
 		};
 
 		mutable std::unique_ptr<RWLock> csConnections;

client/ConnectivityManager.cpp
@@ -50,6 +50,8 @@ class ListenerException : public Exception
 
 ConnectivityManager::ConnectivityManager() : running(0), autoDetect{false, false}, forcePortTest(false)
 {
+	memset(reflectedIP, 0, sizeof(reflectedIP));
+	memset(localIP, 0, sizeof(localIP));
 	mappers[0].init(AF_INET);
 	mappers[1].init(AF_INET6);
 	checkIP6();
@@ -73,9 +75,7 @@ void ConnectivityManager::detectConnection(int af)
 	SettingsManager::set(ips.incomingConnections, SettingsManager::INCOMING_FIREWALL_PASSIVE);
 	listenTCP(af);
 
-	const string ipStr = getLocalIP(af);
-	IpAddress ip;
-	Util::parseIpAddress(ip, ipStr);
+	IpAddress ip = getLocalIP(af);
 	bool isPrivate = false;
 	switch (ip.type)
 	{
@@ -94,7 +94,7 @@ void ConnectivityManager::detectConnection(int af)
 	else
 	{
 		SettingsManager::set(ips.incomingConnections, SettingsManager::INCOMING_DIRECT);
-		log(STRING_F(CONN_DETECT_PUBLIC, ipStr), SEV_INFO, af);
+		log(STRING_F(CONN_DETECT_PUBLIC, Util::printIpAddress(ip)), SEV_INFO, af);
 	}
 }
 
@@ -239,10 +239,10 @@ string ConnectivityManager::getInformation() const
 	string mode = getModeString(AF_INET);
 	s += mode;
 	s += '\n';
-	string externalIP = getReflectedIP(AF_INET);
-	if (!externalIP.empty())
+	IpAddress ip = getReflectedIP(AF_INET);
+	if (!Util::isEmpty(ip))
 	{
-		s += "\t" + STRING_F(CONNECTIVITY_EXTERNAL_IP, 4 % externalIP);
+		s += "\t" + STRING_F(CONNECTIVITY_EXTERNAL_IP, 4 % Util::printIpAddress(ip));
 		s += '\n';
 	}
 	const string& bindV4 = SETTING(BIND_ADDRESS);
@@ -256,10 +256,10 @@ string ConnectivityManager::getInformation() const
 		mode = getModeString(AF_INET6);
 		s += "\t" + mode;
 		s += '\n';
-		externalIP = getReflectedIP(AF_INET6);
-		if (!externalIP.empty())
+		ip = getReflectedIP(AF_INET6);
+		if (!Util::isEmpty(ip))
 		{
-			s += "\t" + STRING_F(CONNECTIVITY_EXTERNAL_IP, 6 % externalIP);
+			s += "\t" + STRING_F(CONNECTIVITY_EXTERNAL_IP, 6 % Util::printIpAddress(ip));
 			s += '\n';
 		}
 		const string& bindV6 = SETTING(BIND_ADDRESS6);
@@ -466,28 +466,42 @@ unsigned ConnectivityManager::getRunningFlags() const noexcept
 	return result;
 }
 
-void ConnectivityManager::setReflectedIP(int af, const string& ip) noexcept
+static int getIndex(int af) noexcept
 {
-	LOCK(cs);
-	reflectedIP[af == AF_INET6 ? 1 : 0] = ip;
+	if (af != AF_INET && af != AF_INET6) return -1;
+	return af == AF_INET6 ? 1 : 0;
 }
 
-string ConnectivityManager::getReflectedIP(int af) const noexcept
+void ConnectivityManager::setReflectedIP(const IpAddress& ip) noexcept
 {
+	int index = getIndex(ip.type);
+	if (index == -1) return;
 	LOCK(cs);
-	return reflectedIP[af == AF_INET6 ? 1 : 0];
+	reflectedIP[index] = ip;
 }
 
-void ConnectivityManager::setLocalIP(int af, const string& ip) noexcept
+IpAddress ConnectivityManager::getReflectedIP(int af) const noexcept
 {
+	int index = getIndex(af);
+	if (index == -1) return IpAddress{};
 	LOCK(cs);
-	localIP[af == AF_INET6 ? 1 : 0] = ip;
+	return reflectedIP[index];
 }
 
-string ConnectivityManager::getLocalIP(int af) const noexcept
+void ConnectivityManager::setLocalIP(const IpAddress& ip) noexcept
 {
+	int index = getIndex(ip.type);
+	if (index == -1) return;
 	LOCK(cs);
-	return localIP[af == AF_INET6 ? 1 : 0];
+	localIP[index] = ip;
+}
+
+IpAddress ConnectivityManager::getLocalIP(int af) const noexcept
+{
+	int index = getIndex(af);
+	if (index == -1) return IpAddress{};
+	LOCK(cs);
+	return localIP[index];
 }
 
 const MappingManager& ConnectivityManager::getMapper(int af) const
@@ -508,9 +522,8 @@ void ConnectivityManager::checkIP6()
 #endif
 		vector<Util::AdapterInfo> adapters;
 		Util::getNetworkAdapters(AF_INET6, adapters);
-		Ip6Address ip;
 		for (const auto& ai : adapters)
-			if (Util::parseIpAddress(ip, ai.ip) && !Util::isReservedIp(ip) && !Util::isLinkScopedIp(ip))
+			if (!Util::isReservedIp(ai.ip.data.v6) && !Util::isLinkScopedIp(ai.ip.data.v6))
 			{
 				result = true;
 				break;

client/ConnectivityManager.h
@@ -21,6 +21,7 @@
 
 #include "Singleton.h"
 #include "MappingManager.h"
+#include "IpAddress.h"
 #include <atomic>
 
 class ConnectivityManager : public Singleton<ConnectivityManager>
@@ -29,10 +30,10 @@ class ConnectivityManager : public Singleton<ConnectivityManager>
 		void setupConnections(bool forcePortTest = false);
 		bool isSetupInProgress() const noexcept { return getRunningFlags() != 0; }
 		void processPortTestResult();
-		void setReflectedIP(int af, const string& ip) noexcept;
-		string getReflectedIP(int af) const noexcept;
-		void setLocalIP(int af, const string& ip) noexcept;
-		string getLocalIP(int af) const noexcept;
+		void setReflectedIP(const IpAddress& ip) noexcept;
+		IpAddress getReflectedIP(int af) const noexcept;
+		void setLocalIP(const IpAddress& ip) noexcept;
+		IpAddress getLocalIP(int af) const noexcept;
 		const MappingManager& getMapper(int af) const;
 		string getInformation() const;
 		static bool isIP6Supported() { return ipv6Supported; }
@@ -58,8 +59,8 @@ class ConnectivityManager : public Singleton<ConnectivityManager>
 		void disconnect();
 		unsigned getRunningFlags() const noexcept;
 
-		string reflectedIP[2];
-		string localIP[2];
+		IpAddress reflectedIP[2];
+		IpAddress localIP[2];
 		MappingManager mappers[2];
 		mutable FastCriticalSection cs;
 		unsigned running;

commit a296f741a9168be55d86f3079d1bcd3846c037b9

Fix bug in printIpAddress introduced in 0447f0b

client/Ip6Address.cpp
@@ -99,7 +99,7 @@ string Util::printIpAddress(const Ip6Address& ip, bool brackets) noexcept
 
 string Util::printIpAddress(const Ip6AddressEx& ip, bool brackets) noexcept
 {
-	if (!ip.scopeId) return printIpAddress(static_cast<const Ip6Address&>(ip));
+	if (!ip.scopeId) return printIpAddress(static_cast<const Ip6Address&>(ip), brackets);
 	string s = printIpAddress(static_cast<const Ip6Address&>(ip));
 	char buf[64];
 	int len = sprintf(buf, "%%%u", ip.scopeId);