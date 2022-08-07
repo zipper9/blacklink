zipper9/blacklink

commit 31368ed2e85d581037d7931dc9db37e48311726b

Clean up UserInfoBaseHandler
Get rid of __if_exists (non-standard MSVC extension)

windows/UserInfoBaseHandler.h
@@ -104,7 +104,7 @@ template<class T, const int options = UserInfoGuiTraits::DEFAULT, class T2 = Use
 class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGuiTraits
 {
 		/*
-		1) If you want to get the features USER_LOG and (or) NICK_TO_CHAT you need to create methods, respectively onOpenUserLog and (or) onAddNickToChat in its class.
+		1) If USER_LOG is set, openUserLog must be implemented; if NICK_TO_CHAT is set, addNickToChat must be implemented
 		2) clearUserMenu()
 		3) reinitUserMenu(user, hint)
 		4) appendAndActivateUserItems(yourMenu)
@@ -152,27 +152,20 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 		COMMAND_ID_HANDLER(IDC_COPY_IP, onCopyUserInfo)
 		COMMAND_ID_HANDLER(IDC_COPY_ALL, onCopyUserInfo)
 		END_MSG_MAP()
-		
-		virtual LRESULT onOpenUserLog(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+
+		LRESULT onOpenUserLog(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 		{
-			dcassert(0);
-			LogManager::message("Not implemented [virtual LRESULT onOpenUserLog]");
-			return 0;
-		}
-		
-		virtual LRESULT onAddNickToChat(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-		{
-			dcassert(0);
-			LogManager::message("Not implemented [virtual LRESULT onAddNickToChat]");
+			static_cast<T*>(this)->openUserLog();
 			return 0;
 		}
 
-		virtual OnlineUserPtr getSelectedOnlineUser() const
+		LRESULT onAddNickToChat(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 		{
-			return OnlineUserPtr();
+			static_cast<T*>(this)->addNickToChat();
+			return 0;
 		}
 
-		virtual LRESULT onCopyUserInfo(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+		LRESULT onCopyUserInfo(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 		{
 			OnlineUserPtr ou = getSelectedOnlineUser();
 			if (ou)
@@ -276,19 +269,18 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			}
 			else
 			{
-				__if_exists(T::getUserList)
+				vector<T2> selected;
+				static_cast<T*>(this)->getSelectedUsers(selected);
+				if (selected.size() > 1)
 				{
-					if (((T*)this)->getUserList().getSelectedCount() > 1)
-					{
-						const tstring message = UserInfoSimple::getBroadcastPrivateMessage();
-						if (!message.empty())
-							((T*)this)->getUserList().forEachSelectedParam(&UserInfoBase::pmText, selectedHint, message);
-					}
-					else
-					{
-						using std::placeholders::_1;
-						((T*)this)->getUserList().forEachSelectedT(std::bind(&UserInfoBase::pm, _1, selectedHint));
-					}
+					const tstring message = UserInfoSimple::getBroadcastPrivateMessage();
+					if (!message.empty())
+						for (const auto& u : selected)
+							UserInfoSimple(u, selectedHint).pmText(selectedHint, message);
+				}
+				else if (!selected.empty())
+				{
+					UserInfoSimple(selected[0], selectedHint).pm(selectedHint);
 				}
 			}
 			return 0;
@@ -341,6 +333,23 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			return 0;
 		}
 
+		OnlineUserPtr getSelectedOnlineUser() const
+		{
+			return OnlineUserPtr();
+		}
+
+		void openUserLog()
+		{
+			dcassert(0);
+			LogManager::message("openUserLog is not implemented");
+		}
+
+		void addNickToChat()
+		{
+			dcassert(0);
+			LogManager::message("addNickToChat is not implemented");
+		}
+
 		const T2& getSelectedUser() const
 		{
 			return selectedUser;
@@ -386,15 +395,14 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			}
 			else
 			{
-				__if_exists(T::getUserList) // ??
+				vector<T2> selected;
+				static_cast<T*>(this)->getSelectedUsers(selected);
+				if (!selected.empty())
 				{
 					FavUserTraits traits; // empty
 					if (ENABLE(options, NICK_TO_CHAT))
-					{
 						menu.AppendMenu(MF_STRING, IDC_ADD_NICK_TO_CHAT, CTSTRING(ADD_NICK_TO_CHAT));
-					}
-					int count = ((T*)this)->getUserList().getSelectedCount();
-					appendSendAutoMessageItems(menu, count);
+					appendSendAutoMessageItems(menu, selected.size());
 					appendFileListItems(menu, traits);
 					appendContactListMenu(menu, traits);
 					appendFavPrivateMenu(menu);
@@ -581,11 +589,10 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			}
 			else
 			{
-				__if_exists(T::getUserList)
-				{
-					using std::placeholders::_1;
-					((T*)this)->getUserList().forEachSelectedT(std::bind(func, _1, data));
-				}
+				vector<T2> selected;
+				static_cast<T*>(this)->getSelectedUsers(selected);
+				for (auto& u : selected)
+					(UserInfoSimple(u, selectedHint).*func)(data);
 			}
 		}
 		
@@ -597,10 +604,10 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			}
 			else
 			{
-				__if_exists(T::getUserList)
-				{
-					((T*)this)->getUserList().forEachSelectedParam(func, hubHint, data);
-				}
+				vector<T2> selected;
+				static_cast<T*>(this)->getSelectedUsers(selected);
+				for (auto& u : selected)
+					(UserInfoSimple(u, selectedHint).*func)(hubHint, data);
 			}
 		}
 		
@@ -612,10 +619,10 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			}
 			else
 			{
-				__if_exists(T::getUserList)
-				{
-					((T*)this)->getUserList().forEachSelectedParam(func, hubHint, data);
-				}
+				vector<T2> selected;
+				static_cast<T*>(this)->getSelectedUsers(selected);
+				for (auto& u : selected)
+					(UserInfoSimple(u, selectedHint).*func)(hubHint, data);
 			}
 		}
 		
@@ -627,21 +634,19 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			}
 			else
 			{
-				__if_exists(T::getUserList)
+				vector<T2> selected;
+				static_cast<T*>(this)->getSelectedUsers(selected);
+				if (!selected.empty())
 				{
-					using std::placeholders::_1;
 					if (useOnlyFirstItem)
-					{
-						((T*)this)->getUserList().forFirstSelectedT(std::bind(func, _1, hubHint));
-					}
+						(UserInfoSimple(selected[0], selectedHint).*func)(hubHint);
 					else
-					{
-						((T*)this)->getUserList().forEachSelectedT(std::bind(func, _1, hubHint));
-					}
+						for (auto& u : selected)
+							(UserInfoSimple(u, selectedHint).*func)(hubHint);
 				}
 			}
 		}
-		
+
 		void doAction(void (UserInfoBase::*func)())
 		{
 			if (selectedUser)
@@ -650,10 +655,10 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			}
 			else
 			{
-				__if_exists(T::getUserList)
-				{
-					((T*)this)->getUserList().forEachSelected(func);
-				}
+				vector<T2> selected;
+				static_cast<T*>(this)->getSelectedUsers(selected);
+				for (auto& u : selected)
+					(UserInfoSimple(u, selectedHint).*func)();
 			}
 		}
 	private:

windows/HubFrame.cpp
@@ -2490,32 +2490,6 @@ LRESULT HubFrame::onSelectUser(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCt
 	return 0;
 }
 
-LRESULT HubFrame::onAddNickToChat(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-{
-	if (isConnected())
-	{
-		if (getSelectedUser())
-		{
-			lastUserName = Text::toT(getSelectedUser()->getIdentity().getNick());
-		}
-		else
-		{
-			lastUserName.clear();
-			const auto& listView = ctrlUsers.getUserList();
-			int i = -1;
-			while ((i = listView.GetNextItem(i, LVNI_SELECTED)) != -1)
-			{
-				if (!lastUserName.empty())
-					lastUserName += _T(", ");
-
-				lastUserName += Text::toT(listView.getItemData(i)->getNick());
-			}
-		}
-		appendNickToChat(lastUserName);
-	}
-	return 0;
-}
-
 LRESULT HubFrame::onAutoScrollChat(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 {
 	ctrlClient.invertAutoScroll();
@@ -2539,25 +2513,6 @@ LRESULT HubFrame::onUnBanIP(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/
 		const string s = "!unban " + Text::fromT(ChatCtrl::g_sSelectedIP);
 		sendMessage(s);
 	}
-	return 0;
-}
-
-LRESULT HubFrame::onOpenUserLog(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-{
-	UserInfo* ui = nullptr;
-	if (getSelectedUser())
-	{
-		ui = ctrlUsers.findUser(getSelectedUser());
-		if (!ui) return 0;
-	}
-
-	StringMap params = getFrameLogParams();
-	
-	params["userNI"] = ui->getNick();
-	params["userCID"] = ui->getUser()->getCID().toBase32();
-	
-	WinUtil::openLog(SETTING(LOG_FILE_PRIVATE_CHAT), params, TSTRING(NO_LOG_FOR_USER));
-	
 	return 0;
 }
 
@@ -2602,6 +2557,48 @@ LRESULT HubFrame::onChatLinkClicked(UINT, WPARAM, LPARAM, BOOL& bHandled)
 	else
 		bHandled = FALSE;
 	return 0;
+}
+
+void HubFrame::addNickToChat()
+{
+	if (isConnected())
+	{
+		if (getSelectedUser())
+		{
+			lastUserName = Text::toT(getSelectedUser()->getIdentity().getNick());
+		}
+		else
+		{
+			lastUserName.clear();
+			const auto& listView = ctrlUsers.getUserList();
+			int i = -1;
+			while ((i = listView.GetNextItem(i, LVNI_SELECTED)) != -1)
+			{
+				if (!lastUserName.empty())
+					lastUserName += _T(", ");
+
+				lastUserName += Text::toT(listView.getItemData(i)->getNick());
+			}
+		}
+		appendNickToChat(lastUserName);
+	}
+}
+
+void HubFrame::openUserLog()
+{
+	UserInfo* ui = nullptr;
+	if (getSelectedUser())
+	{
+		ui = ctrlUsers.findUser(getSelectedUser());
+		if (!ui) return;
+	}
+
+	StringMap params = getFrameLogParams();
+	
+	params["userNI"] = ui->getNick();
+	params["userCID"] = ui->getUser()->getCID().toBase32();
+	
+	WinUtil::openLog(SETTING(LOG_FILE_PRIVATE_CHAT), params, TSTRING(NO_LOG_FOR_USER));
 }
 
 void HubFrame::addDupUsersToSummaryMenu(const ClientManager::UserParams& param, vector<UserInfoGuiTraits::DetailsItem>& detailsItems, UINT& idc)

windows/HubFrame.h
@@ -43,7 +43,7 @@ class HubFrame : public MDITabChildWindowImpl<HubFrame>,
 	public CSplitterImpl<HubFrame>,
 	public CMessageFilter,
 	public UCHandler<HubFrame>,
-	public UserInfoBaseHandler < HubFrame, UserInfoGuiTraits::NO_CONNECT_FAV_HUB | UserInfoGuiTraits::NICK_TO_CHAT | UserInfoGuiTraits::USER_LOG | UserInfoGuiTraits::INLINE_CONTACT_LIST, OnlineUserPtr >,
+	public UserInfoBaseHandler<HubFrame, UserInfoGuiTraits::NO_CONNECT_FAV_HUB | UserInfoGuiTraits::NICK_TO_CHAT | UserInfoGuiTraits::USER_LOG | UserInfoGuiTraits::INLINE_CONTACT_LIST, OnlineUserPtr>,
 	private SettingsManagerListener,
 	private FavoriteManagerListener,
 	private UserManagerListener,
@@ -112,7 +112,6 @@ class HubFrame : public MDITabChildWindowImpl<HubFrame>,
 		COMMAND_ID_HANDLER(IDC_BAN_IP, onBanIP)
 		COMMAND_ID_HANDLER(IDC_UNBAN_IP, onUnBanIP)
 		COMMAND_ID_HANDLER(IDC_OPEN_HUB_LOG, onOpenHubLog)
-		COMMAND_ID_HANDLER(IDC_OPEN_USER_LOG, onOpenUserLog)
 		COMMAND_ID_HANDLER(IDC_COPY_HUBNAME, onCopyHubInfo)
 		COMMAND_ID_HANDLER(IDC_COPY_HUBADDRESS, onCopyHubInfo)
 		COMMAND_ID_HANDLER(IDC_COPY_HUB_IP, onCopyHubInfo)
@@ -146,12 +145,10 @@ class HubFrame : public MDITabChildWindowImpl<HubFrame>,
 		LRESULT onReconnect(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onDisconnect(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onSelectUser(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
-		LRESULT onAddNickToChat(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onAutoScrollChat(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onBanIP(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onUnBanIP(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onOpenHubLog(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
-		LRESULT onOpenUserLog(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onSizeMove(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*/, BOOL& bHandled);
 		LRESULT onChatLinkClicked(UINT, WPARAM, LPARAM, BOOL&);
 
@@ -236,6 +233,12 @@ class HubFrame : public MDITabChildWindowImpl<HubFrame>,
 			}
 			return 0;
 		}
+
+		// UserInfoBaseHandler
+		OnlineUserPtr getSelectedOnlineUser() const { return getSelectedUser(); }
+		void getSelectedUsers(vector<OnlineUserPtr>& v) const { ctrlUsers.getSelectedUsers(v); }
+		void addNickToChat();
+		void openUserLog();
 
 	private:
 		enum AutoConnectType
@@ -354,9 +357,6 @@ class HubFrame : public MDITabChildWindowImpl<HubFrame>,
 		void on(ClientListener::StatusMessage, const Client*, const string& line, int statusFlags) noexcept override;
 		void on(SettingsLoaded, const Client*) noexcept override;
 
-		// UserInfoBaseHandler
-		OnlineUserPtr getSelectedOnlineUser() const override { return getSelectedUser(); }
-
 		// UserListWindow::HubFrameCallbacks
 		void showErrorMessage(const tstring& text) override;
 		void setCurrentNick(const tstring& nick) override;
@@ -386,7 +386,6 @@ class HubFrame : public MDITabChildWindowImpl<HubFrame>,
 		{
 			WinUtil::openLog(SETTING(LOG_FILE_MAIN_CHAT), getFrameLogParams(), TSTRING(NO_LOG_FOR_HUB));
 		}
-		UserListWindow::CtrlUsers& getUserList() { return ctrlUsers.getUserList(); }
 
 	protected:
 		// BaseChatFrame

windows/PrivateFrame.h
@@ -82,7 +82,6 @@ class PrivateFrame : public MDITabChildWindowImpl<PrivateFrame>,
 		COMMAND_ID_HANDLER(IDC_CLOSE_ALL_OFFLINE_PM, onCloseAllOffline)
 		COMMAND_ID_HANDLER(IDC_CLOSE_ALL_PM, onCloseAll)
 		COMMAND_ID_HANDLER(IDC_CLOSE_WINDOW, onCloseWindow)
-		COMMAND_ID_HANDLER(IDC_OPEN_USER_LOG, onOpenUserLog)
 		COMMAND_ID_HANDLER(IDC_SELECT_HUB, onShowHubMenu);
 		COMMAND_ID_HANDLER(IDC_CCPM, onCCPM);
 		CHAIN_COMMANDS(ucBase)
@@ -100,11 +99,6 @@ class PrivateFrame : public MDITabChildWindowImpl<PrivateFrame>,
 		LRESULT onTabContextMenu(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM lParam, BOOL& /*bHandled*/);
 		LRESULT onContextMenu(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
 		LRESULT onTabGetOptions(UINT, WPARAM, LPARAM lParam, BOOL&);
-		LRESULT onOpenUserLog(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-		{
-			openFrameLog();
-			return 0;
-		}
 		LRESULT onShowHubMenu(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onCCPM(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
 		LRESULT onLButton(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled); // !Decker!
@@ -173,6 +167,11 @@ class PrivateFrame : public MDITabChildWindowImpl<PrivateFrame>,
 		bool selectHub(const string& url);
 
 		static PrivateFrame* findFrameByID(uint64_t id);
+
+		// UserInfoBaseHandler
+		OnlineUserPtr getSelectedOnlineUser() const;
+		void getSelectedUsers(vector<UserPtr>& v) const {}
+		void openUserLog() { openFrameLog(); }
 
 	private:
 		enum
@@ -272,9 +271,6 @@ class PrivateFrame : public MDITabChildWindowImpl<PrivateFrame>,
 		void readFrameLog() override;
 
 		StringMap getFrameLogParams() const;
-
-		// UserInfoBaseHandler
-		OnlineUserPtr getSelectedOnlineUser() const override;
 
 	public:
 		void createMessagePanel();

windows/SearchFrm.cpp
@@ -1242,8 +1242,8 @@ void SearchFrame::SearchInfo::Download::operator()(SearchInfo* si)
 			QueueManager::getInstance()->add(target, si->sr.getSize(), si->sr.getTTH(), si->sr.getHintedUser(), mask, prio, true, getConnFlag);
 			si->sr.flags |= SearchResult::FLAG_QUEUED;
 			sf->updateList = true;
-			
-			const auto children = sf->getUserList().findChildren(si->getGroupCond());
+
+			const auto children = sf->ctrlResults.findChildren(si->getGroupCond());
 			for (auto i = children.cbegin(); i != children.cend(); ++i)
 			{
 				SearchInfo* j = *i;
@@ -2950,6 +2950,17 @@ void SearchFrame::on(SettingsManagerListener::Repaint)
 		ctrlSearchFilterTree.SetTextColor(Colors::g_textColor);
 #endif
 		RedrawWindow(NULL, NULL, RDW_ERASE | RDW_INVALIDATE | RDW_UPDATENOW | RDW_ALLCHILDREN);
+	}
+}
+
+void SearchFrame::getSelectedUsers(vector<UserPtr>& v) const
+{
+	int i = ctrlResults.GetNextItem(-1, LVNI_SELECTED);
+	while (i >= 0)
+	{
+		SearchInfo* si = ctrlResults.getItemData(i);
+		v.push_back(si->getUser());
+		i = ctrlResults.GetNextItem(i, LVNI_SELECTED);
 	}
 }
 

windows/SearchFrm.h
@@ -50,7 +50,8 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 	private TimerHelper,
 	public CMessageFilter
 {
-		friend class DirectoryListingFrame;
+		class SearchInfo;
+		typedef TypedTreeListViewCtrl<SearchInfo, IDC_RESULTS, TTHValue> SearchInfoList;
 
 	public:
 		static void openWindow(const tstring& str = Util::emptyStringT, LONGLONG size = 0, SizeModes mode = SIZE_ATLEAST, int type = FILE_TYPE_ANY);
@@ -257,17 +258,11 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 			closeAll();
 			return 0;
 		}
-		
+
+		void getSelectedUsers(vector<UserPtr>& v) const;
+
 	private:
 		bool getDownloadDirectory(WORD wID, tstring& dir) const;
-		class SearchInfo;
-		
-	public:
-		typedef TypedTreeListViewCtrl<SearchInfo, IDC_RESULTS, TTHValue> SearchInfoList;
-		SearchInfoList& getUserList()
-		{
-			return ctrlResults;
-		}
 
 	private:
 		enum
@@ -313,22 +308,15 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 					ipUpdated = false;
 #endif
 				}
-				~SearchInfo()
-				{
-				}
-				
-				const UserPtr& getUser() const override
-				{
-					return sr.getUser();
-				}
+				const UserPtr& getUser() const override { return sr.getUser(); }
 				bool collapsed;
 				SearchInfo* parent;
 				size_t hits;
 				int iconIndex;
-				
+
 				void getList();
 				void browseList();
-				
+
 				struct Download
 				{
 					Download(const tstring& target, SearchFrame* sf, QueueItem::Priority prio, QueueItem::MaskType mask = 0) : tgt(target), sf(sf), prio(prio), mask(mask) { }

windows/TransferView.cpp
@@ -2245,6 +2245,17 @@ void TransferView::onTimerInternal()
 	processTasks();
 }
 
+void TransferView::getSelectedUsers(vector<UserPtr>& v) const
+{
+	int i = ctrlTransfers.GetNextItem(-1, LVNI_SELECTED);
+	while (i >= 0)
+	{
+		ItemInfo* ii = ctrlTransfers.getItemData(i);
+		v.push_back(ii->getUser());
+		i = ctrlTransfers.GetNextItem(i, LVNI_SELECTED);
+	}
+}
+
 TransferView::ItemInfo::ItemInfo() : hintedUser(HintedUser(nullptr, Util::emptyString)), download(true)
 {
 	init();

windows/TransferView.h
@@ -54,7 +54,10 @@ class TransferView : public CWindowImpl<TransferView>, private DownloadManagerLi
 		
 		typedef UserInfoBaseHandler<TransferView, UserInfoGuiTraits::NO_COPY> uibBase;
 		typedef UCHandler<TransferView> ucBase;
-		
+
+		class ItemInfo;
+		typedef TypedTreeListViewCtrl<ItemInfo, IDC_TRANSFERS, tstring> ItemInfoList;
+
 		BEGIN_MSG_MAP(TransferView)
 		NOTIFY_HANDLER(IDC_TRANSFERS, LVN_GETDISPINFO, ctrlTransfers.onGetDispInfo)
 		NOTIFY_HANDLER(IDC_TRANSFERS, LVN_COLUMNCLICK, ctrlTransfers.onColumnClick)
@@ -195,15 +198,12 @@ class TransferView : public CWindowImpl<TransferView>, private DownloadManagerLi
 		}
 		
 	public:
-		class ItemInfo;
-		typedef TypedTreeListViewCtrl<ItemInfo, IDC_TRANSFERS, tstring> ItemInfoList;
-		ItemInfoList& getUserList()
-		{
-			return ctrlTransfers;
-		}
+		void getSelectedUsers(vector<UserPtr>& v) const;
+
 #ifdef IRAINMAN_ENABLE_WHOIS
 		tstring selectedIP;
 #endif
+
 	private:
 		enum Tasks
 		{

commit b4745daf4d3527dfb94f22076a34d9d5b69137d9

Fix Copy menu in HubFrame (broken in 31368ed2)

windows/UserInfoBaseHandler.h
@@ -167,7 +167,7 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 
 		LRESULT onCopyUserInfo(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 		{
-			OnlineUserPtr ou = getSelectedOnlineUser();
+			OnlineUserPtr ou = static_cast<T*>(this)->getSelectedOnlineUser();
 			if (ou)
 			{
 				const Identity& id = ou->getIdentity();
@@ -424,7 +424,7 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			UserInfoSimple ui(selectedUser, hint);
 			FavUserTraits traits;
 			traits.init(ui);
-			OnlineUserPtr ou = getSelectedOnlineUser();
+			OnlineUserPtr ou = static_cast<T*>(this)->getSelectedOnlineUser();
 			if (ou) addSummaryMenu(ou);
 			if (ENABLE(options, NICK_TO_CHAT))
 			{
@@ -534,7 +534,7 @@ class UserInfoBaseHandler : UserInfoBaseHandlerTraitsUser<T2>, public UserInfoGu
 			dcassert(selectedUser);
 			if (DISABLE(options, NO_COPY))
 			{
-				int flags = getSelectedOnlineUser() ? MF_ENABLED : MF_GRAYED;
+				int flags = static_cast<T*>(this)->getSelectedOnlineUser() ? MF_ENABLED : MF_GRAYED;
 				int count = copyUserMenu.GetMenuItemCount();
 				for (int i = 1; i < count; i++)
 					copyUserMenu.EnableMenuItem(i, MF_BYPOSITION | flags);