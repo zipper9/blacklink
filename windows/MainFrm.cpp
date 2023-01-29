zipper9/blacklink

commit 3ea82ad47a33b5b675e6b68448c1a708d66d46ad

Improvements in SearchManager and FinishedManager

windows/MainFrm.cpp
@@ -67,7 +67,7 @@
 #include "../client/LogManager.h"
 #include "../client/WebServerManager.h"
 #include "../client/ThrottleManager.h"
-#include "../client/Text.h"
+#include "../client/Random.h"
 #include "../client/NmdcHub.h"
 #include "../client/dht/DHT.h"
 #include "../client/DCPlusPlus.h"
@@ -342,7 +342,8 @@ LRESULT MainFrame::onCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/,
 	UserManager::getInstance()->addListener(this);
 	FinishedManager::getInstance()->addListener(this);
 	FavoriteManager::getInstance()->addListener(this);
-	
+	SearchManager::getInstance()->addListener(this);
+
 	if (SETTING(NICK).empty())
 	{
 		ShowWindow(SW_RESTORE);
@@ -2156,6 +2157,7 @@ LRESULT MainFrame::onClose(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, 
 				transferView.prepareClose();
 
 				WebServerManager::getInstance()->removeListener(this);
+				SearchManager::getInstance()->removeListener(this);
 				FavoriteManager::getInstance()->removeListener(this);
 				FinishedManager::getInstance()->removeListener(this);
 				UserManager::getInstance()->removeListener(this);
@@ -2715,6 +2717,19 @@ void MainFrame::on(QueueManagerListener::Finished, const QueueItemPtr& qi, const
 				WinUtil::openFile(Text::toT(qi->getTarget()));
 			}
 		}
+	}
+}
+
+void MainFrame::on(SearchManagerListener::SR, const SearchResult& sr) noexcept
+{
+	if (sr.isAutoToken()) return;
+	uint64_t id = SearchTokenList::instance.getTokenOwner(sr.getToken());
+	if (!id) return;
+	SearchFrame* frame = SearchFrame::getFramePtr(id);
+	if (frame)
+	{
+		frame->addSearchResult(sr);
+		SearchFrame::releaseFramePtr(frame);
 	}
 }
 

windows/SearchFrm.cpp
@@ -41,6 +41,8 @@
 #define USE_DOWNLOAD_DIR
 
 SearchHistory SearchFrame::lastSearches;
+SearchFrame::FrameMap SearchFrame::activeFrames;
+CriticalSection SearchFrame::framesLock;
 
 static const unsigned SEARCH_RESULTS_WAIT_TIME = 10000;
 
@@ -159,8 +161,6 @@ static const ResourceManager::Strings hubsColumnNames[] =
 	ResourceManager::TIME_TO_WAIT
 };
 
-SearchFrame::FrameMap SearchFrame::g_search_frames;
-
 template<typename C>
 bool isTTH(const std::basic_string<C>& s)
 {
@@ -169,6 +169,7 @@ bool isTTH(const std::basic_string<C>& s)
 }
 
 SearchFrame::SearchFrame() :
+	id(WinUtil::getNewFrameID(WinUtil::FRAME_TYPE_SEARCH)),
 	TimerHelper(m_hWnd),
 	showUIContainer(WC_COMBOBOX, this, SHOWUI_MESSAGE_MAP),
 #ifdef BL_FEATURE_IP_DATABASE
@@ -216,26 +217,43 @@ SearchFrame::~SearchFrame()
 {
 	dcassert(everything.empty());
 	dcassert(closed);
+	searchParam.removeToken();
 	images.Destroy();
 	searchTypesImageList.Destroy();
 }
 
 void SearchFrame::openWindow(const tstring& str /* = Util::emptyString */, LONGLONG size /* = 0 */, SizeModes mode /* = SIZE_ATLEAST */, int type /* = FILE_TYPE_ANY */)
 {
-	SearchFrame* pChild = new SearchFrame();
-	pChild->setInitial(str, size, mode, type);
-	pChild->Create(WinUtil::g_mdiClient);
-	g_search_frames.insert(FramePair(pChild->m_hWnd, pChild));
+	SearchFrame* frame = new SearchFrame();
+	frame->setInitial(str, size, mode, type);
+	frame->Create(WinUtil::g_mdiClient);
+	framesLock.lock();
+	activeFrames.insert(make_pair(frame->id, frame));
+	framesLock.unlock();
 }
 
 void SearchFrame::closeAll()
 {
-	dcdrun(const auto frameCount = g_search_frames.size());
-	for (auto i = g_search_frames.cbegin(); i != g_search_frames.cend(); ++i)
+	LOCK(framesLock);
+	for (auto& frame : activeFrames)
+		::PostMessage(frame.second->m_hWnd, WM_CLOSE, 0, 0);
+}
+
+SearchFrame* SearchFrame::getFramePtr(uint64_t id)
+{
+	framesLock.lock();
+	auto i = activeFrames.find(id);
+	if (i == activeFrames.end())
 	{
-		::PostMessage(i->first, WM_CLOSE, 0, 0);
+		framesLock.unlock();
+		return nullptr;
 	}
-	dcassert(frameCount == g_search_frames.size());
+	return i->second;
+}
+
+void SearchFrame::releaseFramePtr(SearchFrame* frame)
+{
+	if (frame) framesLock.unlock();
 }
 
 LRESULT SearchFrame::onFiletypeChange(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
@@ -553,7 +571,6 @@ LRESULT SearchFrame::onCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*
 	tooltip.SetMaxTipWidth(200);
 	tooltip.Activate(TRUE);
 	onSizeMode();   //Get Mode, and turn ON or OFF controlls Size
-	SearchManager::getInstance()->addListener(this);
 	FavoriteManager::getInstance()->addListener(this);
 	initHubs();
 	treeItemRoot = nullptr;
@@ -603,8 +620,7 @@ LRESULT SearchFrame::onMeasure(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bH
 		CustomDrawHelpers::measureComboBox(reinterpret_cast<MEASUREITEMSTRUCT*>(lParam), Fonts::g_systemFont);
 		return TRUE;
 	}
-	HWND hwnd = 0; // ???
-	return OMenu::onMeasureItem(hwnd, uMsg, wParam, lParam, bHandled);
+	return OMenu::onMeasureItem(m_hWnd, uMsg, wParam, lParam, bHandled);
 }
 
 LRESULT SearchFrame::onDrawItem(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
@@ -617,8 +633,7 @@ LRESULT SearchFrame::onDrawItem(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& b
 	}
 	if (dis->CtlType == ODT_MENU)
 	{
-		HWND hwnd = 0; // ???
-		return OMenu::onDrawItem(hwnd, uMsg, wParam, lParam, bHandled);
+		return OMenu::onDrawItem(m_hWnd, uMsg, wParam, lParam, bHandled);
 	}
 	if (dis->CtlID == ATL_IDW_STATUS_BAR && dis->itemID == STATUS_PROGRESS)
 	{
@@ -734,9 +749,10 @@ void SearchFrame::onEnter()
 	unsigned scale = 1u << (ctrlSizeMode.GetCurSel() * 10);
 	size *= scale;
 	searchParam.size = size;
-	
+	searchParam.owner = id;
+
 	search = StringTokenizer<string>(Text::fromT(s), ' ').getTokens();
-	
+
 	string filter;
 	string filterExclude;
 	{
@@ -770,6 +786,7 @@ void SearchFrame::onEnter()
 			}
 			++si;
 		}
+		searchParam.removeToken();
 		searchParam.generateToken(false);
 	}
 	
@@ -806,7 +823,7 @@ void SearchFrame::onEnter()
 	SetWindowText((TSTRING(SEARCH) + _T(" - ") + searchTarget).c_str());
 	
 	// stop old search
-	ClientManager::cancelSearch(this);
+	ClientManager::cancelSearch(id);
 	searchParam.extList.clear();
 	// Get ADC searchtype extensions if any is selected
 	try
@@ -835,7 +852,6 @@ void SearchFrame::onEnter()
 		searchParam.filter = filter;
 		searchParam.filterExclude = filterExclude;
 		searchParam.normalizeWhitespace();
-		searchParam.owner = this;
 		if (portStatus == PortTest::STATE_FAILURE || BOOLSETTING(SEARCH_PASSIVE)) // || (SETTING(OUTGOING_CONNECTIONS) == SettingsManager::OUTGOING_SOCKS5);
 			searchParam.searchMode = SearchParamBase::MODE_PASSIVE;
 		else
@@ -858,24 +874,20 @@ void SearchFrame::removeSelected()
 	}
 }
 
-void SearchFrame::on(SearchManagerListener::SR, const SearchResult& sr) noexcept
+void SearchFrame::addSearchResult(const SearchResult& sr)
 {
 	if (isClosedOrShutdown())
 		return;
 	// Check that this is really a relevant search result...
 	{
 		LOCK(csSearch);
-		
 		if (search.empty())
 			return;
-			
-		needUpdateResultCount = true;
+
 		if (sr.getToken() && searchParam.token != sr.getToken())
-		{
-			droppedResults++;
 			return;
-		}
-		
+
+		needUpdateResultCount = true;
 		string ext;
 #if 0
 		bool isExecutable = false;
@@ -1546,8 +1558,9 @@ LRESULT SearchFrame::onClose(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/
 		SettingsManager::getInstance()->removeListener(this);
 		ClientManager::getInstance()->removeListener(this);
 		FavoriteManager::getInstance()->removeListener(this);
-		SearchManager::getInstance()->removeListener(this);
-		g_search_frames.erase(m_hWnd);
+		framesLock.lock();
+		activeFrames.erase(id);
+		framesLock.unlock();
 		ctrlResults.deleteAll();
 		ctrlHubs.deleteAll();
 		clearFound();

windows/SearchFrm.h
@@ -46,7 +46,6 @@
 #define SEARCH_FILTER_MESSAGE_MAP 11
 
 class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
-	private SearchManagerListener,
 	private ClientManagerListener,
 	private FavoriteManagerListener,
 	public UCHandler<SearchFrame>, public UserInfoBaseHandler<SearchFrame, UserInfoGuiTraits::NO_COPY>,
@@ -258,7 +257,11 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 			return 0;
 		}
 
+		void addSearchResult(const SearchResult& sr);
 		void getSelectedUsers(vector<UserPtr>& v) const;
+
+		static SearchFrame* getFramePtr(uint64_t id);
+		static void releaseFramePtr(SearchFrame* frame);
 
 	private:
 		bool getDownloadDirectory(WORD wID, tstring& dir) const;
@@ -297,9 +300,6 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 		class SearchInfo : public UserInfoBase
 		{
 			public:
-				typedef SearchInfo* Ptr;
-				typedef vector<Ptr> Array;
-				
 				SearchInfo(const SearchResult &sr) : sr(sr), collapsed(true), parent(nullptr),
 					colMask(0), hits(0), iconIndex(-1)
 				{
@@ -405,6 +405,7 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 			STATUS_LAST
 		};
 
+		const uint64_t id;
 		tstring initialString;
 		int64_t initialSize;
 		SizeModes initialMode;
@@ -513,7 +514,7 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 		bool hasWaitTime;
 		bool startingSearch;
 		
-		SearchParamToken searchParam;
+		SearchParam searchParam;
 		vector<SearchClientItem> searchClients;
 		bool useDHT;
 		int64_t exactSize;
@@ -538,12 +539,12 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 		StringMap ucLineParams;
 		
 		static const int columnId[];
-		
-		typedef std::map<HWND, SearchFrame*> FrameMap;
-		typedef pair<HWND, SearchFrame*> FramePair;
-		
-		static FrameMap g_search_frames;
-		
+
+		typedef std::map<uint64_t, SearchFrame*> FrameMap;
+
+		static FrameMap activeFrames;
+		static CriticalSection framesLock;
+
 		struct DownloadTarget
 		{
 			enum DefinedTypes
@@ -573,10 +574,6 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 		int makeTargetMenu(const SearchInfo* si, OMenu& menu, int idc, ResourceManager::Strings title);
 		static tstring getTargetDirectory(const SearchInfo* si, const tstring& downloadDir);
 
-		void on(SearchManagerListener::SR, const SearchResult& sr) noexcept override;
-		
-		//void on(SearchManagerListener::Searching, SearchQueueItem* aSearch) noexcept override;
-		
 		// ClientManagerListener
 		void on(ClientConnected, const Client* c) noexcept override
 		{

client/AdcHub.cpp
@@ -1233,7 +1233,7 @@ StringList AdcHub::parseSearchExts(int flag)
 	return ret;
 }
 
-void AdcHub::searchToken(const SearchParamToken& sp)
+void AdcHub::searchToken(const SearchParam& sp)
 {
 	{
 		LOCK(csState);

client/AdcHub.h
@@ -37,7 +37,7 @@ class AdcHub : public Client, public CommandHandler<AdcHub>
 		void hubMessage(const string& message, bool thirdPerson = false) override;
 		bool privateMessage(const OnlineUserPtr& user, const string& message, bool thirdPerson, bool automatic) override;
 		void sendUserCmd(const UserCommand& command, const StringMap& params) override;
-		void searchToken(const SearchParamToken& sp) override;
+		void searchToken(const SearchParam& sp) override;
 		void password(const string& pwd, bool setPassword) override;
 		void info(bool forceUpdate) override;
 		void refreshUserList(bool) override;

client/Client.cpp
@@ -29,6 +29,7 @@
 #include "Wildcards.h"
 #include "ParamExpander.h"
 #include "Resolver.h"
+#include "Random.h"
 
 std::atomic<uint32_t> Client::g_counts[COUNT_UNCOUNTED];
 
@@ -609,7 +610,7 @@ bool Client::allowNatTraversal()
 	return BOOLSETTING(ALLOW_NAT_TRAVERSAL) && SETTING(OUTGOING_CONNECTIONS) == SettingsManager::OUTGOING_DIRECT;
 }
 
-unsigned Client::searchInternal(const SearchParamToken& sp)
+unsigned Client::searchInternal(const SearchParam& sp)
 {
 	if (searchQueue.interval)
 	{
@@ -691,7 +692,7 @@ void Client::on(Second, uint64_t tick) noexcept
 		Search s;
 		if (searchQueue.pop(s, tick))
 		{
-			SearchParamToken sp;
+			SearchParam sp;
 			sp.token = s.token;
 			sp.sizeMode = s.sizeMode;
 			sp.fileType = s.fileType;
@@ -700,7 +701,7 @@ void Client::on(Second, uint64_t tick) noexcept
 			sp.filterExclude = std::move(s.filterExclude);
 			sp.searchMode = s.searchMode;
 			sp.extList = std::move(s.extList);
-			sp.owner = nullptr;
+			sp.owner = 0;
 			searchToken(sp);
 		}
 	}

client/Client.h
@@ -85,10 +85,10 @@ class Client : public ClientBase,
 		virtual void hubMessage(const string& aMessage, bool thirdPerson = false) = 0;
 		virtual void sendUserCmd(const UserCommand& command, const StringMap& params) = 0;
 		
-		unsigned searchInternal(const SearchParamToken& sp);
-		void cancelSearch(void* aOwner)
+		unsigned searchInternal(const SearchParam& sp);
+		void cancelSearch(uint64_t owner)
 		{
-			searchQueue.cancelSearch(aOwner);
+			searchQueue.cancelSearch(owner);
 		}
 		virtual void password(const string& pwd, bool setPassword) = 0;
 		virtual void info(bool forceUpdate) = 0;
@@ -455,7 +455,7 @@ class Client : public ClientBase,
 		/** Reload details from favmanager or settings */
 		void reloadSettings(bool updateNick);
 
-		virtual void searchToken(const SearchParamToken& sp) = 0;
+		virtual void searchToken(const SearchParam& sp) = 0;
 		virtual void onTimer(uint64_t tick) noexcept {}
 
 		// TimerManagerListener

client/ClientManager.cpp
@@ -973,7 +973,7 @@ void ClientManager::on(AdcSearch, const Client* c, const AdcCommand& adc, const 
 	}
 }
 
-void ClientManager::search(const SearchParamToken& sp)
+void ClientManager::search(const SearchParam& sp)
 {
 	{
 		READ_LOCK(*g_csClients);
@@ -988,7 +988,7 @@ void ClientManager::search(const SearchParamToken& sp)
 		dht::DHT::getInstance()->findFile(sp.filter, sp.token, sp.owner);
 }
 
-unsigned ClientManager::multiSearch(const SearchParamToken& sp, vector<SearchClientItem>& clients)
+unsigned ClientManager::multiSearch(const SearchParam& sp, vector<SearchClientItem>& clients)
 {
 	unsigned maxWaitTime = 0;
 	bool useDHT = false;
@@ -1194,7 +1194,7 @@ bool ClientManager::isActive(int af, int favHubMode)
 	return getConnectivityMode(af, favHubMode) != SettingsManager::INCOMING_FIREWALL_PASSIVE;
 }
 
-void ClientManager::cancelSearch(void* owner)
+void ClientManager::cancelSearch(uint64_t owner)
 {
 	READ_LOCK(*g_csClients);
 	for (auto i = g_clients.cbegin(); i != g_clients.cend(); ++i)

commit a2669c3deb9701fe0d0633816a0e74d1b3a28f30

Fix NMDC search (broken in 3ea82ad4)

windows/MainFrm.cpp
@@ -2722,6 +2722,12 @@ void MainFrame::on(QueueManagerListener::Finished, const QueueItemPtr& qi, const
 
 void MainFrame::on(SearchManagerListener::SR, const SearchResult& sr) noexcept
 {
+	if (!sr.getToken())
+	{
+		SearchFrame::broadcastSearchResult(sr);
+		return;
+	}
+
 	if (sr.isAutoToken()) return;
 	uint64_t id = SearchTokenList::instance.getTokenOwner(sr.getToken());
 	if (!id) return;

windows/SearchFrm.cpp
@@ -256,6 +256,14 @@ void SearchFrame::releaseFramePtr(SearchFrame* frame)
 	if (frame) framesLock.unlock();
 }
 
+void SearchFrame::broadcastSearchResult(const SearchResult& sr)
+{
+	framesLock.lock();
+	for (auto& frame : activeFrames)
+		frame.second->addSearchResult(sr);
+	framesLock.unlock();
+}
+
 LRESULT SearchFrame::onFiletypeChange(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 {
 	if (BOOLSETTING(SAVE_SEARCH_SETTINGS))

windows/SearchFrm.h
@@ -262,6 +262,7 @@ class SearchFrame : public MDITabChildWindowImpl<SearchFrame>,
 
 		static SearchFrame* getFramePtr(uint64_t id);
 		static void releaseFramePtr(SearchFrame* frame);
+		static void broadcastSearchResult(const SearchResult& sr);
 
 	private:
 		bool getDownloadDirectory(WORD wID, tstring& dir) const;